d3768bb294d76d3110a54ae5d4b89132dc38e027
Roman Afanaskin <raafanaskin@avito.ru>
2020-03-01 21:18:34 +0300

    step03 - извлечение данных об игроках

--- scraper/engine.py
+++ scraper/engine.py
@@ -6,13 +6,12 @@ import requests
 
 def start(start_url: str, callback: typing.Callable):
     start_task = (start_url, callback)
-    urls = deque([start_task])
+    tasks = deque([start_task])
 
-    while urls:
-        url, callback = urls.popleft()
+    while tasks:
+        url, callback = tasks.popleft()
         print(url)
         resp = requests.get(url)
 
-        next_task = callback(resp)
-        if next_task:
-            urls.append(next_task)
+        for task in callback(resp):
+            tasks.append(task)
--- scraper/spiders/fifa.py
+++ scraper/spiders/fifa.py
@@ -1,19 +1,35 @@
 import requests
+from parsel import Selector
 
 
 BASE_URL = 'https://www.fifaindex.com'
 
 
 def parse(resp: requests.Response):
-    index = resp.text.find('Next Page')
-    if index == -1:
-        return
+    sel = Selector(resp.text)
 
-    url = []
-    for ch in resp.text[index - 3::-1]:
-        if ch == '"':
-            break
-        url.append(ch)
+    player_css = 'td[data-title="Name"] a::attr(href)'
+    tasks = [(BASE_URL + url, parse_player) for url in sel.css(player_css).getall()]
 
-    assert url, 'blank url'
-    return BASE_URL + ''.join(url[::-1]), parse
+    next_xpath = '//a[text()="Next Page"]/@href'
+    next_page = sel.xpath(next_xpath).get()
+    if next_page:
+        tasks.append((BASE_URL + next_page, parse))
+
+    return tasks
+
+
+def parse_player(resp: requests.Response):
+    sel = Selector(resp.text)
+    card = sel.css('.row .pt-3 .card')
+
+    player = {'name': card.css('h5::text').get()}
+
+    metrics = ['Height', 'Weight', 'Age']
+    for name in metrics:
+        metric_xpath = f'//p[text()="{name} "]/span//text()'
+        val_str = card.xpath(metric_xpath).get().split()[0]
+        player[name.lower()] = int(val_str)
+
+    print(player)
+    return []
